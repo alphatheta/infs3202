3e59a84d1b58c1b66e03d407ce799247656790df
Ameer Sabri <s4204082@GS336-4144.labs.eait.uq.edu.au>
2015-06-05 12:51:15 +1000

    fix in db line, retry

--- add_comment.php
+++ add_comment.php
@@ -1,7 +1,7 @@
 <?php
 	$q = $_GET['q'];
 	$t = $_GET['t'];
-	$conn = mysqli_connect('xdsv8dafke.database.windows.net,1433', 'asabri', '8377394201w$', 'dbo');
+	$conn = mysqli_connect('xdsv8dafke.database.windows.net,1433', 'asabri', '8377394201w$', 'infs3202db');
 	
 	if (!$conn) {
 		die('Could not connect: ' . mysqli_error($conn));
