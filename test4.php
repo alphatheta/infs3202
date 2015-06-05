eb2222dc6c841ffd1b47741f5b4980af79ea6f12
Ameer Sabri <s4204082@GS336-4144.labs.eait.uq.edu.au>
2015-06-05 13:24:31 +1000

    test with just variables no pdo

--- test4.php
+++ test4.php
@@ -12,15 +12,7 @@
 	$pwd = "8377394201w$";
 	$db = "infs3202db";
 
-	echo "test1";
-	try{
-		$conn = new PDO( "sqlsrv:Server= $server; Database = $db", $user, $pwd);
-		$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-	}
-	catch(Exception $e){
-		die(print_r($e));
-	}
-	echo "test2";
+	echo "testnew";
 	?>
 </body>
 </html>
