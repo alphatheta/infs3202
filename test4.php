8daaef6929c443c3f46942a6fddeca61e264e831
Ameer Sabri <s4204082@GS336-4144.labs.eait.uq.edu.au>
2015-06-05 13:17:01 +1000

    yet another test

--- test4.php
+++ test4.php
@@ -1,5 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<meta charset="UTF-8">
+	<title>test page</title>
+</head>
+<body>
+	<?php
+	$server = "tcp:xdsv8dafke.database.windows.net,1433";
+	$user = "asabri"@xdsv8dafke;
+	$pwd = "8377394201w$";
+	$db = "infs3202db";
 
-echo "testing to ensure this works at all";
-
-?>
+	echo "test1";
+	try{
+		$conn = new PDO( "sqlsrv:Server= $server; Database = $db", $user, $pwd);
+		$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	}
+	catch(Exception $e){
+		die(print_r($e));
+	}
+	echo "test2";
+	?>
+</body>
