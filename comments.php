2c6d5cc0fb00e9723d4f790229c4e53e219966ad
Ameer Sabri <s4204082@GS336-4144.labs.eait.uq.edu.au>
2015-06-05 14:26:53 +1000

    adaptation of code for pdo

--- comments.php
+++ comments.php
@@ -1,19 +1,26 @@
 <?php
 	$q = $_GET['q'];
-	$conn = mysqli_connect('xdsv8dafke.database.windows.net,1433', 'asabri', '8377394201w$', 'infs3202db');
+	$server = "tcp:xdsv8dafke.database.windows.net,1433";
+	$user = "asabri@xdsv8dafke";
+	$pwd = "8377394201w$";
+	$db = "infs3202db";
 	
-	if (!$conn) {
-		die('Could not connect:gtestest ' . mysqli_error($conn));
+	try{
+		$conn = new PDO( "sqlsrv:Server= $server; Database = $db", $user, $pwd);
+		$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	}
+	catch(Exception $e){
+		die(print_r($e));
 	}
 
-	$query="SELECT * FROM `dbo`.`Comments` WHERE place_id = '".$q."'";
-	$result = mysqli_query($conn, $query);
+	$query="SELECT * FROM dbo.`Comments WHERE place_id = '".$q."'";
+	$result = $conn->query($query);
 
 	echo "<h3>Comments</h3>";
 
-	while($row = mysqli_fetch_array($result)) {
+	while($row = $result->fetch(PDO::FETCH_ASSOC)) {
 		echo "<li class=\"comments\">" . $row['text'] . "</li>";
 	}
 	
-	mysqli_close($conn);
+	$conn = null;
 ?>
