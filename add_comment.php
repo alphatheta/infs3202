bb811f36508e82130b17cb7b647d8de819a5ab15
Ameer Sabri <s4204082@GS336-4144.labs.eait.uq.edu.au>
2015-06-05 12:49:44 +1000

    fix in mysqli_connect, reverted removal of graves

--- add_comment.php
+++ add_comment.php
@@ -1,13 +1,13 @@
 <?php
 	$q = $_GET['q'];
 	$t = $_GET['t'];
-	$conn = mysqli_connect('tcp:xdsv8dafke.database.windows.net,1433', 'asabri', '8377394201w$', 'dbo');
+	$conn = mysqli_connect('xdsv8dafke.database.windows.net,1433', 'asabri', '8377394201w$', 'dbo');
 	
 	if (!$conn) {
 		die('Could not connect: ' . mysqli_error($conn));
 	}
 
-	$query="INSERT INTO dbo.Comments (place_id, text) VALUES ('$q', '$t')";
+	$query="INSERT INTO `dbo`.`Comments` (`place_id`, `text`) VALUES ('$q', '$t')";
 	mysqli_query($conn, $query);
 
 	echo "<li class=\"comments\">" . $t . "</li>";
