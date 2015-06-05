e550a4a84260037b84c33a1000a3c30eb291e212
Ameer Sabri <s4204082@GS336-4144.labs.eait.uq.edu.au>
2015-06-05 12:55:22 +1000

    actually changed the connection details on the relevant code

--- comments.php
+++ comments.php
@@ -1,12 +1,12 @@
 <?php
 	$q = $_GET['q'];
-	$conn = mysqli_connect('localhost', 'admin', 'password', 'comments');
+	$conn = mysqli_connect('xdsv8dafke.database.windows.net,1433', 'asabri', '8377394201w$', 'infs3202db');
 	
 	if (!$conn) {
 		die('Could not connect: ' . mysqli_error($conn));
 	}
 
-	$query="SELECT * FROM `comments`.`Comments` WHERE place_id = '".$q."'";
+	$query="SELECT * FROM `dbo`.`Comments` WHERE place_id = '".$q."'";
 	$result = mysqli_query($conn, $query);
 
 	echo "<h3>Comments</h3>";
