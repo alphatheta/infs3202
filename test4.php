b73d554e498c51c7fcc437ca38cd43e284a3008f
Ameer Sabri <s4204082@GS336-4144.labs.eait.uq.edu.au>
2015-06-05 13:27:34 +1000

    another quick test

--- test4.php
+++ test4.php
@@ -6,13 +6,6 @@
 	<title>test page</title>
 </head>
 <body>
-	<?php
-	//$server = "tcp:xdsv8dafke.database.windows.net,1433";
-	//$user = "asabri@xdsv8dafke";
-	//$pwd = "8377394201w$";
-	//$db = "infs3202db";
 
-	echo "testnew";
-	?>
 </body>
 </html>
