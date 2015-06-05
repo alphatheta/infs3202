66d76d3af3d0fbe85a069ed878e55c02ea732d95
Ameer Sabri <s4204082@GS336-4144.labs.eait.uq.edu.au>
2015-06-05 13:19:12 +1000

    retest for responsiveness

--- test4.php
+++ test4.php
@@ -6,21 +6,6 @@
 	<title>test page</title>
 </head>
 <body>
-<!--
-	<?php
-	$server = "tcp:xdsv8dafke.database.windows.net,1433";
-	$user = "asabri"@xdsv8dafke;
-	$pwd = "8377394201w$";
-	$db = "infs3202db";
 
-	echo "test1";
-	try{
-		$conn = new PDO( "sqlsrv:Server= $server; Database = $db", $user, $pwd);
-		$conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-	}
-	catch(Exception $e){
-		die(print_r($e));
-	}
-	echo "test2";
-	?> -->
+	<p>sample content</p>
 </body>
