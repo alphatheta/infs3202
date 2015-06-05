ef6087e561399b4260a65b14da4a5650bb093c13
Ameer Sabri <s4204082@GS336-4144.labs.eait.uq.edu.au>
2015-06-05 14:48:13 +1000

    test

--- counter.php
+++ counter.php
@@ -8,6 +8,13 @@
 <body>
 	<h1>This page refreshes every 2 seconds!</h1>
 	<?php
+		$count_my_page = ("counter.txt");
+		$hits = file($count_my_page);
+		$hits[0] ++;
+		$fp = fopen($count_my_page , "w");
+		fputs($fp , "$hits[0]");
+		fclose($fp);
+		echo $hits[0];
 		header("Refresh: 2;");
 	?>
 </body>
